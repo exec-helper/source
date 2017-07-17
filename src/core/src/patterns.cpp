bf1453f908e17d391692c32092f238316b51a6f5
Bart Verhagen <barrie.verhagen@gmail.com>
2017-07-17 17:25:22 +0200

    Fixed issues reported by coverity that were relevant to the exec-helper source code

--- src/core/src/patterns.cpp
+++ src/core/src/patterns.cpp
@@ -5,12 +5,18 @@
 using std::string;
 using std::regex;
 using std::regex_replace;
+using std::regex_error;
 
 namespace execHelper { namespace core {
     string replacePatterns(const string& subject, const string& pattern, const string& replacement) noexcept {
         const string PATTERN_PREFIX(R"(\{)");
         const string PATTERN_POSTFIX(R"(\})");
-        return regex_replace(subject, regex(PATTERN_PREFIX + pattern + PATTERN_POSTFIX), replacement);
+        try {
+            return regex_replace(subject, regex(PATTERN_PREFIX + pattern + PATTERN_POSTFIX), replacement);
+        } catch(regex_error&) {
+            user_feedback("System warning: invalid regex for " << pattern);
+            return subject;
+        }
     }
 } // namespace core
 } // namespace execHelper
