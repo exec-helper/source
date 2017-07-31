bd38dbb7d815547ebe9a5827dd93e25e1fb06c0c
Bart Verhagen <barrie.verhagen@gmail.com>
2017-07-31 10:39:29 +0200

    New: Doc: Added documentation for the command plugin.

--- src/commander/include/commander/commander.h
+++ src/commander/include/commander/commander.h
@@ -19,13 +19,37 @@ namespace execHelper {
 
 namespace execHelper {
     namespace commander {
+        /**
+         * Class that will read the commands to be run and kicks all the plugins related to these commands
+         */
         class Commander {
             public:
+                /**
+                 * Creates a commander
+                 *
+                 * \param options   The command line options
+                 * \param env       The environment to apply the plugins in
+                 */
                 Commander(const core::Options& options, core::EnvironmentCollection&& env = core::EnvironmentCollection());
 
+                /**
+                 * Run the commander
+                 *
+                 * \returns True    If the command was run successfully
+                 *          False   Otherwise
+                 */
                 bool run() noexcept;
                 
             private:
+                /**
+                 * Applies the plugin associated with the given plugin name
+                 * 
+                 * \param pluginName    The name of the plugin to execute
+                 * \param command       The command that is currently being run
+                 * \param options       The options associated with the command being run
+                 * \returns True    If the plugin was successfully applied
+                 *          False   Otherwise
+                 */
                 bool executePlugin(const std::string& pluginName, const std::string& command, const core::Options& options) noexcept;
 
                 const core::Options& m_options;
