74c4b1c5ad6674787b59d9554405f704954586a0
Bart Verhagen <barrie.verhagen@gmail.com>
2017-08-07 15:09:11 +0200

    Fix: Dev: Fixed issue where gitchangelog installation was not properly detected.

--- cmake/modules/FindGitchangelog.cmake
+++ cmake/modules/FindGitchangelog.cmake
@@ -1,3 +1,11 @@
 find_program(GITCHANGELOG NAMES gitchangelog PATHS /bin /usr/bin /usr/local/bin)
 
 mark_as_advanced(GITCHANGELOG)
+
+if(GITCHANGELOG-NOTFOUND)
+    MESSAGE(STATUS "Did not find gitchangelog")
+    SET(GITCHANGELOG_FOUND OFF)
+else()
+    MESSAGE(STATUS "Found gitchangelog: " ${GITCHANGELOG})
+    SET(GITCHANGELOG_FOUND ON)
+endif()
