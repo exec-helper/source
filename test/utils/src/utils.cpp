bec47ade69c6ce3f4c3bf79fd601d1b05e48928a
Bart Verhagen <bart.verhagen@esaturnus.com>
2017-11-07 09:45:29 +0100

    Fix: Dev: Experiment for fixing a coverity remark in an other way. !minor

--- test/utils/src/utils.cpp
+++ test/utils/src/utils.cpp
@@ -182,14 +182,11 @@ namespace execHelper { namespace test { namespace utils {
         }
         for(const auto& subKey : settings.values()) {
             if(settings[subKey].values().empty()) {
+                assert(yaml.isValid());
                 if(settings.values().size() == 1U) {
                     yaml = subKey;
                 } else {
-                    try {
-                        yaml.push_back(subKey);
-                    } catch(const YAML::InvalidNode&) {
-                        assert(false);
-                    }
+                    yaml.push_back(subKey);
                 }
             } else {
                 try {
