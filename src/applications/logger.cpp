0f7af157d0e978be24633544c8639d33326d0d28
Bart Verhagen <barrie.verhagen@gmail.com>
2020-03-30 21:46:34 +0200

    New: Dev: Added lang-server command to exec-helper config. !minor

---
+++ src/applications/logger.cpp
@@ -0,0 +1,7 @@
+#include "logger.h"
+
+BOOST_LOG_GLOBAL_LOGGER_INIT( // NOLINT(modernize-use-trailing-return-type)
+    exec_helper_application_logger, execHelper::log::LoggerType) {
+    return execHelper::log::LoggerType(boost::log::keywords::channel =
+                                           LOG_CHANNEL);
+}
---
+++ src/applications/logger.h
@@ -0,0 +1,14 @@
+#ifndef LOGGER_INCLUDE
+#define LOGGER_INCLUDE
+
+#include "log/log.h"
+BOOST_LOG_GLOBAL_LOGGER(exec_helper_application_logger, execHelper::log::LoggerType)
+
+static const std::string LOG_CHANNEL = "application";
+#define LOG(x)                                                                 \
+    BOOST_LOG_STREAM_CHANNEL_SEV(exec_helper_application_logger::get(),             \
+                                 LOG_CHANNEL, execHelper::log::x)              \
+        << boost::log::add_value(fileLog, __FILE__)                            \
+        << boost::log::add_value(lineLog, __LINE__)
+
+#endif /* LOGGER_INCLUDE */
